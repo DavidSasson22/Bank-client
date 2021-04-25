c8c929544750ae9cfe99b8cd3198d9232f8e50cd
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-04-25 09:31:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, } from 'react-router-dom';
+import SingleUser from './Pages/singleUser/SingleUser'
 import Admin from './Pages/allUsers/Admin';
-import SingleUser from './Pages/singleUser/SingleUser';
 
 
 export default function App() {
